e87fa56577fa2319c9372576c579a95fd9941f5d
Md. Intikhab Shahriar Hasan <45317472+shasan7@users.noreply.github.com>
2025-09-27 19:06:08 +0600

    Update udenseformer.py

--- Torchvision/udenseformer.py
+++ Torchvision/udenseformer.py
@@ -540,8 +540,8 @@ class MaxVitBlock(nn.Module):
         for idx, p in enumerate(p_stochastic):
             self.layers += [
                 MaxVitLayer(
-                    in_channels=in_channels if idx == 0 else in_channels + idx * growth_rate if mode == "encode" else in_channels - idx * growth_rate,
-                    out_channels=in_channels + idx * growth_rate if mode == "encode" else in_channels - idx * growth_rate,
+                    in_channels=in_channels if idx == 0 else in_channels + idx * growth_rate if mode == "encode" else in_channels - 3 * idx * growth_rate,
+                    out_channels=in_channels + idx * growth_rate if mode == "encode" else in_channels - 3 * idx * growth_rate,
                     growth_rate=growth_rate,
                     bn_size=bn_size,
                     stride=1,
@@ -559,8 +559,7 @@ class MaxVitBlock(nn.Module):
             ]
 
         if pool:
-            self.layers += [nn.Sequential(NormActivationConv(in_channels + n_layers * growth_rate, out_channels, kernel_size=1, stride=1, padding=0),
-                                         nn.MaxPool2d(kernel_size=2, stride=2),)]
+            self.layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             self.grid_size = (self.grid_size[0]//2, self.grid_size[1]//2)
 
     def forward(self, x: Tensor) -> Tensor:
